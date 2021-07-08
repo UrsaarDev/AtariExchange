0bc2a34724b95d8a88233e728fbbdd63c65fcfd6
dash <dash@mail.ru>
2021-07-08 17:30:39 -0400

    change2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Secondpage from './pages/Secondpage';
+import Firstpage from './pages/Firstpage';
 import './App.scss';
 
 class App extends Component {
   render() {
     return  <Router>
               <Switch>
-                <Route exact path="/" component={Secondpage} />
+                <Route exact path="/" component={Firstpage} />
               </Switch>
             </Router>;
   }
--- src/pages/Firstpage/index.js
+++ src/pages/Firstpage/index.js
@@ -1,78 +1,199 @@
 
-import { Menu, MenuItem } from "@material-ui/core";
-import { ExpandMore } from "@material-ui/icons";
+import { Grid, Box, Button, Radio } from "@material-ui/core";
 import React from 'react';
 import { baseUrl } from '../../constants';
 import './style.scss';
 
 function Firstpage() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [periodValue, setPeriod] = React.useState('Buy unlocked ATRI');
+    const [paymentValue, setPaymentMethod] = React.useState('ETH');
+    const [amountValue, setAmount] = React.useState('0');
+    const [cryptoAmountValue, setCryptoAmount] = React.useState('0');
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
+    const handleChangePaymentMethod = (event) => {
+        setPaymentMethod(event.target.value);
     };
-
-    const handleClose = () => {
-        setAnchorEl(null);
+    const handleChangePeriod = (event) => {
+        setPeriod(event.target.value);
+    };
+    const handleChangeAmount = (event) => {
+        setAmount(event.target.value);
+        setCryptoAmount('0.000000');
     };
 
     return (
-        <div className='container'>
-            <div className='topbar'></div>
-            <div className='header'>
-                <div style={{flexGrow:2}}></div>
-                <div className='header-search' style={{flexGrow:7,display:'flex',justifyContent:'flex-end'}}>
-                    <input type='text' />
-                    <span>Search</span>
-                </div>
-                <div className='header-connect' style={{flexGrow:2,display:'flex',justifyContent:'center'}}>
-                    <div className='btn-exchange' onClick={handleClick}>
-                        <span className='exc-label'>Exchange</span> 
-                        <ExpandMore className='exc-icon' size='small' />
-                    </div>
-                    <span className='btn-connect'>Connect</span>
-                </div>
-                <div style={{flexGrow:2}}></div>
-            </div>
-            <div className='content'>
-                <div className='content-three-divs'>
-                    <div className='total-pending'>
-                        <div className='total-div'>
-                            <article>total divs</article>
-                            <div className='gradient-div'></div>
-                        </div>
-                        <div className='blank'></div>
-                        <div className='pending-div'>
-                            <article>pending divs</article>
-                            <div className='gradient-div'></div>
-                        </div>
-                    </div>
-                    <div className='queue-position'>
-                        <div className='blank'></div>
-                        <div className='queue-div'>
-                            <article>queue position</article>
-                            <div className='gradient-div'></div>
-                        </div>
-                        <div className='blank'></div>
-                    </div>
-                </div>
-                <div className='content-logo'>
-                    <div><img className='logo' alt='' src={baseUrl + 'image/logo.png'} /></div>
-                    <div></div>
-                </div>
-            </div>
-            <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-            >
-                <MenuItem className='menu-item' onClick={handleClose}>pcs v1</MenuItem>
-                <MenuItem className='menu-item' onClick={handleClose}>pcs v2</MenuItem>
-                <MenuItem className='menu-item' onClick={handleClose}>ape swap</MenuItem>
-            </Menu>
-        </div>
+        <Box className='container'>
+            <Box className='header'>
+                <Box className='logo'>
+                    <a href='/'>
+                        <img alt='' src={baseUrl + 'forsecond/logo.png'} />
+                    </a>
+                    <Button className='font2' variant="outlined" color="secondary">
+                        connect
+                    </Button>
+                </Box>
+            </Box>
+            <Box className='content'>
+                <Grid container>
+                    <Grid item xs={12} sm={1} md={1} lg={2}></Grid>
+                    <Grid item xs={12} sm={10} md={10} lg={8}>
+                        <Box>
+                            <Box>
+                                <Grid className='card-two' container spacing={3}>
+                                    <Grid item xs={12} sm={12} md={6}>
+                                        <Box className='card-header font3'>
+                                            <Box component='span'>AVAILABLE BALANCE</Box>
+                                            <img alt='' className='action' src={baseUrl + 'forsecond/action1.png'} />
+                                        </Box>
+                                        <Box className='card-content'>
+                                            <Box className='grey-border1' mt={2} pb={1.3}>
+                                                <Box className='font4' component='span'>Available ETH balance:</Box>
+                                                <Box className='card-input' component='span'>0</Box>
+                                            </Box>
+                                            <Box className='grey-border1' mt={2} pb={1.3}>
+                                                <Box className='font4' component='span'>Available USDT balance:</Box>
+                                                <Box className='card-input' component='span'>0</Box>
+                                            </Box>
+                                            <Box className='grey-border1' mt={2} pb={1.3}>
+                                                <Box className='font4' component='span'>Available ATRI balance:</Box>
+                                                <Box className='card-input' component='span'>0</Box>
+                                            </Box>
+                                        </Box>
+                                    </Grid>
+                                    <Grid item xs={12} sm={12} md={6}>
+                                        <Box className='card-header font3'>
+                                            <Box component='span'>YOUR TRANSACTION</Box>
+                                            <img alt='' className='action' src={baseUrl + 'forsecond/action2.png'} />
+                                        </Box>
+                                        <Box className='card-content'>
+                                            <Box className='grey-border1' mt={2} pb={1.3}>
+                                                <Box className='font4' component='span'>Amount in ATRI:</Box>
+                                                <input className='card-input font4 text-right' defaultValue='0' onChange={handleChangeAmount}/>
+                                            </Box>
+                                            <Box className='grey-border1' mt={2} pb={1.3}>
+                                                <Box className='font4' component='span'>Payment method:</Box>
+                                                <Box className='float-right radio-group font4'>
+                                                    <Radio
+                                                        checked={paymentValue === 'ETH'}
+                                                        onChange={handleChangePaymentMethod}
+                                                        value="ETH"
+                                                        name="radio-button-demo"
+                                                        inputProps={{ 'aria-label': 'A' }}
+                                                    />
+                                                    <Box component='span'>ETH</Box>
+                                                    <Radio
+                                                        checked={paymentValue === 'USDT'}
+                                                        onChange={handleChangePaymentMethod}
+                                                        value="USDT"
+                                                        name="radio-button-demo"
+                                                        inputProps={{ 'aria-label': 'B' }}
+                                                    />
+                                                    <Box component='span'>USDT</Box>
+                                                </Box>
+                                            </Box>
+                                            <Box className='grey-border1' mt={2} pb={1.3}>
+                                                <Box className='font4' component='span'>Amount in crypto:</Box>
+                                                <Box className='float-right' component='span'>
+                                                    <Box component='span'>{cryptoAmountValue}</Box>{'\u00a0'}
+                                                    <Box className='eth-string' component='span'>{paymentValue}</Box>
+                                                </Box>
+                                            </Box>
+                                        </Box>
+                                    </Grid>
+                                </Grid>
+                                <Box className='card-period'>
+                                    <Box className='card-header font3'>
+                                        <Box component='span'>SELECT YOUR LOCKING PERIOD</Box>
+                                        <img alt='' className='action' src={baseUrl + 'forsecond/action3.png'} />
+                                    </Box>
+                                    <Grid className='grey-border2' container spacing={3}>
+                                        <Grid item md={6}>
+                                            <Radio
+                                                checked={periodValue === 'Buy unlocked ATRI'}
+                                                onChange={handleChangePeriod}
+                                                value="Buy unlocked ATRI"
+                                                name="radio-button-demo"
+                                                inputProps={{ 'aria-label': 'A' }}
+                                            />
+                                            <Box className='font4' component='span' pl={2.4}>Buy unlocked ATRI</Box>
+                                        </Grid>
+                                        <Grid className='text-right' item xs={12} sm={12} md={6}>
+                                            <Box className='font4' component='span' >You will receive {amountValue} ATRI</Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid className='grey-border2' container spacing={3}>
+                                        <Grid item md={6}>
+                                            <Radio
+                                                checked={periodValue === 'Lockup 1 month'}
+                                                onChange={handleChangePeriod}
+                                                value="Lockup 1 month"
+                                                name="radio-button-demo"
+                                                inputProps={{ 'aria-label': 'A' }}
+                                            />
+                                            <Box className='font4' component='span' pl={2.4}>Lockup 1 month</Box>
+                                        </Grid>
+                                        <Grid className='text-right' item xs={12} sm={12} md={6}>
+                                            <Box className='font4' component='span' >You will receive {parseInt(amountValue)} ATRI</Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid className='grey-border2' container spacing={3}>
+                                        <Grid item md={6}>
+                                            <Radio
+                                                checked={periodValue === 'Lockup 3 months'}
+                                                onChange={handleChangePeriod}
+                                                value="Lockup 3 months"
+                                                name="radio-button-demo"
+                                                inputProps={{ 'aria-label': 'A' }}
+                                            />
+                                            <Box className='font4' component='span' pl={2.4}>Lockup 3 months</Box>
+                                        </Grid>
+                                        <Grid className='text-right' item xs={12} sm={12} md={6}>
+                                            <Box className='font4' component='span' >You will receive {parseInt(amountValue)} ATRI</Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid className='grey-border2' container spacing={3}>
+                                        <Grid item md={6}>
+                                            <Radio
+                                                checked={periodValue === 'Lockup 6 months'}
+                                                onChange={handleChangePeriod}
+                                                value="Lockup 6 months"
+                                                name="radio-button-demo"
+                                                inputProps={{ 'aria-label': 'A' }}
+                                            />
+                                            <Box className='font4' component='span' pl={2.4}>Lockup 6 months</Box>
+                                        </Grid>
+                                        <Grid className='text-right' item xs={12} sm={12} md={6}>
+                                            <Box className='font4' component='span' >You will receive {parseInt(amountValue)} ATRI</Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid className='grey-border2' container spacing={3}>
+                                        <Grid item md={6}>
+                                            <Radio
+                                                checked={periodValue === 'Lockup 12 months'}
+                                                onChange={handleChangePeriod}
+                                                value="Lockup 12 months"
+                                                name="radio-button-demo"
+                                                inputProps={{ 'aria-label': 'A' }}
+                                            />
+                                            <Box className='font4' component='span' pl={2.4}>Lockup 12 months</Box>
+                                        </Grid>
+                                        <Grid className='text-right' item xs={12} sm={12} md={6}>
+                                            <Box className='font4' component='span' >You will receive {parseInt(amountValue)} ATRI</Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Box className='text-center' mt={1}>
+                                        <button className='submit-btn font1'>
+                                            BUY
+                                        </button>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={1} md={1} lg={2}></Grid>
+                </Grid>
+            </Box>
+        </Box>
     );
 }
 
